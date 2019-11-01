cd49a07e64860d246abeb144c278be9a445024dc
Ali Zaynoune <alzaynou@e1r7p1.1337.ma>
2019-11-01 15:14:48 +0100

    microb1

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: alzaynou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/31 13:09:51 by alzaynou          #+#    #+#             */
-/*   Updated: 2019/11/01 14:22:07 by alzaynou         ###   ########.fr       */
+/*   Updated: 2019/11/01 15:12:08 by alzaynou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/ft_printf.h"
+//#include "../includes/ft_printf.h"
 #include <stdio.h>
-
+/*
 //void	ft_list_arg(const char *format)
 
 int     ft_printf(const char *format, ...)
@@ -47,20 +47,17 @@ int     ft_read_format(const char *str)
 		if (str[cnt] == '%' && (CNTT 'c' || CNTT 's' || CNTT 'p' || CNTT 'h' ||
 					CNTT 'l' || CNTT 'L'))
 			flags.carg++;
-	/*	if (str[cnt] '%' && (CNTT ' ' || CNTT '	'))
-		{
-			if (cnt > 0 && (
-	*/	cnt++;
+		cnt++;
 	}
 
 	return (flags.carg);
-}
+}*/
 int		main()
 {
 //	float a = 1.8888888;    
 	size_t s = 9;
 	void *a = &s;
-	ft_printf(" %s %s %s %s %s %s %s %s %d", 9, 2, 5, 6,10 , 221);
-	printf(" |%19p|\n",a);
+//	ft_printf(" %s %s %s %s %s %s %s %s %d", 9, 2, 5, 6,10 , 221);
+	printf("");
 	return (0);
 }
