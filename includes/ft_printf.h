c9a75a1620600f88003b8a0b8f5df5d66165a0fd
Ali Zaynoune <alzaynou@e1r7p1.1337.ma>
2019-11-01 18:00:02 +0100

    microb0

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: alzaynou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/31 13:06:55 by alzaynou          #+#    #+#             */
-/*   Updated: 2019/11/01 14:58:59 by alzaynou         ###   ########.fr       */
+/*   Updated: 2019/11/01 17:47:39 by alzaynou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,26 @@
 # include "../libft/libft.h"
 # include<stdarg.h>
 /*
-** 
+** Macros
 */
-#define CNTT str[cnt + 1] ==
+#define CNTP str[cnt + 1] ==
+#define CNTN str[cnt - 1] ==
+#define C c ==
+#define TYP(typ)  typ > 0 ? char : int
+//#define TYP int
+/*
+** Data stuct
+*/
 
 typedef struct s_flags
 {
 	int		carg;
-	char	*arg;
+	char	*str;
 }				t_flags;
 
 int     ft_printf(const char *format, ...);
 int     ft_read_format(const char *str);
 void    ft_list_arg(const char *format);
+int     ft_check_flags(char c);
+void    ft_print(int a, int typ);
 #endif
--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,19 +6,18 @@
 /*   By: alzaynou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/31 13:09:51 by alzaynou          #+#    #+#             */
-/*   Updated: 2019/11/01 15:12:08 by alzaynou         ###   ########.fr       */
+/*   Updated: 2019/11/01 17:52:29 by alzaynou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "../includes/ft_printf.h"
+#include "../includes/ft_printf.h"
 #include <stdio.h>
-/*
-//void	ft_list_arg(const char *format)
 
 int     ft_printf(const char *format, ...)
 {
 	int		cnt;
 	int		carg;
+	int		typ = 0;
 	va_list ap;
 
 	cnt = 0;
@@ -26,7 +25,7 @@ int     ft_printf(const char *format, ...)
 	va_start(ap, format);
 	while (cnt < carg)
 	{
-		ft_putnbr(va_arg(ap, int));
+		ft_print(typ, (va_arg(ap, int)));
 		ft_putchar('\n');
 		cnt++;
 	}
@@ -44,20 +43,35 @@ int     ft_read_format(const char *str)
 
 	while (str[cnt])
 	{
-		if (str[cnt] == '%' && (CNTT 'c' || CNTT 's' || CNTT 'p' || CNTT 'h' ||
-					CNTT 'l' || CNTT 'L'))
+		if (str[cnt] == '%' && (ft_check_flags(str[cnt + 1])))
 			flags.carg++;
 		cnt++;
 	}
 
 	return (flags.carg);
-}*/
+}
+
+int		ft_check_flags(char c)
+{
+	if (C 'd' || C 'i' || C 'o' || C 'u' || C 'x' || C 'c' || C 's' || C 'p'||
+			C 'l' || C 'L' || C 'h' || C 'f' || C '%')
+		return (1);
+	return (0);
+}
+
+void	ft_print(int a, int typ)
+{
+	if (a == 0)
+		ft_putnbr(typ);
+	if (a == 1)
+		ft_putchar(typ);
+}
 int		main()
 {
 //	float a = 1.8888888;    
-	size_t s = 9;
-	void *a = &s;
-//	ft_printf(" %s %s %s %s %s %s %s %s %d", 9, 2, 5, 6,10 , 221);
-	printf("");
+//	size_t s = 9;
+//	void *a = &s;
+	ft_printf(" %s %s %s %s %s %s", 9, 2, 5, 6,10 , 221);
+//	printf("");
 	return (0);
 }
