b1e5de09da1acdee1ef997ff2e9b8c135ad767d9
Ali Zaynoune <alzaynou@e1r7p1.1337.ma>
2019-11-01 12:05:43 +0100

    microbe

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: alzaynou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/31 13:09:51 by alzaynou          #+#    #+#             */
-/*   Updated: 2019/11/01 11:44:44 by alzaynou         ###   ########.fr       */
+/*   Updated: 2019/11/01 12:05:15 by alzaynou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 //void	ft_list_arg(const char *format)
 
-/*int     ft_printf(const char *format, ...)
+int     ft_printf(const char *format, ...)
 {
 	int		cnt;
 	int		carg;
@@ -34,7 +34,7 @@
 	return (0);
 }
 
-t_flags     ft_read_format(const char *str)
+int     ft_read_format(const char *str)
 {
 	int		cnt;
 	t_flags flags;
@@ -44,24 +44,23 @@ t_flags     ft_read_format(const char *str)
 
 	while (str[cnt])
 	{
-		if (str[cnt] '%' && (CNT+ 'c' || CNTT 's' || CNTT 'p' || CNTT 'h' ||
+		if (str[cnt] == '%' && (CNTT 'c' || CNTT 's' || CNTT 'p' || CNTT 'h' ||
 					CNTT 'l' || CNTT 'L'))
 			flags.carg++;
-		if (str[cnt] '%' && (CNTT ' ' || CNTT '	'))
+	/*	if (str[cnt] '%' && (CNTT ' ' || CNTT '	'))
 		{
 			if (cnt > 0 && (
-		cnt++;
+	*/	cnt++;
 	}
 
-	return (flags);
+	return (flags.carg);
 }
-*/
 int		main()
 {
-	float a = 1.8888888;    
+//	float a = 1.8888888;    
 //	size_t s = 9;
 //	void *a = &s;
-	//ft_printf("%csp %+5% %q%%", 9, 2, 5i + 5, 6,10 , 221);
-	printf("\n %.2f \n", a);
+	ft_printf("%s %s %s %s %s %s", 9, 2, 5, 6,10 , 221);
+//	printf("\n %.2f \n", a);
 	return (0);
 }
